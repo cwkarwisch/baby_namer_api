7ea8511f7774bb97be3ef382b33dd4a879438d64
Clay Karwisch <clay.karwisch@gmail.com>
2021-02-17 16:44:41 -0500

    add tests for names controller and name model

--- test/controllers/names_controller_test.rb
+++ test/controllers/names_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class NamesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get names_url
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+  end
+
+  test "get female names" do
+    get names_url, params: { sex: "F" }
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+    assert_includes response.body, "Olivia"
+    assert_includes response.body, "Ariadne"
+    refute_includes response.body, "Liam"
+    refute_includes response.body, "Jermaine"
+  end
+
+
+  test "get male names" do
+    get names_url, params: { sex: "M" }
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+    assert_includes response.body, "Liam"
+    assert_includes response.body, "Jermaine"
+    refute_includes response.body, "Olivia"
+    refute_includes response.body, "Ariadne"
+  end
+
+  test "get female names from 2019" do
+    get names_url, params: { sex: "F", year: "2019"}
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+    assert_includes response.body, "2019"
+    refute_includes response.body, "2018"
+  end
+
+  test "get top 10 female names from 2019" do
+    get names_url, params: { sex: "F", year: "2019", popularity: "10"}
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.content_type
+    assert_includes response.body, "2019"
+    refute_includes response.body, "2018"
+    assert_includes response.body, "Olivia"
+    refute_includes response.body, "Ariadne"
+  end
 end
