19d943efb7a6d45ff152244430a2eb5fb0380a80
Clay Karwisch <clay.karwisch@gmail.com>
2021-02-16 20:28:14 -0500

    parse query parameters and return filtered results

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,6 +1,24 @@
 class NamesController < ApplicationController
   def index
-    @names = Name.all
-    render json: @names
+    if query_parameters?
+      @names = filter_names
+      render json: @names
+    else
+      @names = Name.all
+      render json: @names
+    end
+  end
+
+  private
+
+  def query_parameters?
+    params[:sex] || params[:popularity] || params[:year]
+  end
+
+  def filter_names
+    names = Name.all
+    names = names.where(sex: params[:sex]) if params[:sex]
+    names = names.where(year: params[:year].to_i) if params[:year]
+    names = names.where("popularity <= ?", params[:popularity]) if params[:popularity]
   end
 end
