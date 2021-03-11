38b9f2613fd9ae6ebd37ab35fc2fe5d58ef16edd
Clay Karwisch <clay.karwisch@gmail.com>
2021-03-11 12:16:20 -0500

    update schema to add Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_032514) do
+ActiveRecord::Schema.define(version: 2021_03_10_190758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_03_04_032514) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name", "sex", "popularity", "year"], name: "index_names_on_name_and_sex_and_popularity_and_year"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
