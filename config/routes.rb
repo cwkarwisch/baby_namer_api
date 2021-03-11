2ef4c5e78463309b615e5e67a3e62b9fe6845079
Clay Karwisch <clay.karwisch@gmail.com>
2021-03-11 12:15:23 -0500

    add routes for users resource, login and auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :names, only: :index
+  post '/login', to: 'auth#login'
+  get '/auth',   to: 'auth#persist'
 end
